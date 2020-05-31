265940e92a473e849ee9f627c9382f21b857a0c6
realgam3 <realgam3@gmail.com>
2020-05-31 12:14:17 +0300

    Better Monkeypatch

--- requests_raw/__init__.py
+++ requests_raw/__init__.py
@@ -8,9 +8,19 @@ from .__version__ import (__version__, __build__,
                           __author__, __author_email__,
                           __url__, __license__, __copyright__)
 
+# Original session request function
+_request = requests.Session.request
+
 
 def monkey_patch_all():
+    def __request(self, method, url, *args, **kwargs):
+        if not getattr(self, "_raw_adapter_mounted", False):
+            self.mount("http://", RawAdapter())
+            self.mount("https://", RawAdapter())
+            setattr(self, "_raw_adapter_mounted", True)
+        return _request(self, method, url, *args, **kwargs)
+
     setattr(requests, "raw", raw)
     setattr(requests.api, "raw", raw)
-    setattr(requests, "Session", Session)
-    setattr(requests.sessions, "Session", Session)
+    setattr(requests.sessions.Session, "raw", Session.raw)
+    setattr(requests.sessions.Session, "request", __request)
