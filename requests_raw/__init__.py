0a387219a8945420e26ddee30a5a9d8cc51bb136
Tomer Zait <realgam3@gmail.com>
2021-05-24 20:09:37 +0300

    v1.0.0
    Fixes Bug https://github.com/realgam3/requests-raw/issues/1
    Add automatic patch monkey patch to original requests lib

--- requests_raw/__init__.py
+++ requests_raw/__init__.py
@@ -1,16 +1,41 @@
 import requests
-from .api import raw
 from requests import *
-from .sessions import Session
+from .api import request, raw
 from .adapters import RawAdapter
+from .sessions import Session, session
 from .__version__ import (__version__, __build__,
                           __title__, __description__,
                           __author__, __author_email__,
                           __url__, __license__, __copyright__)
 
+# Original session request function
+_request = requests.Session.request
+
+
+# Fixes Bug https://github.com/realgam3/requests-raw/issues/1
+def __is_response_to_head(response):
+    method = response._method or __title__
+    if isinstance(method, int):
+        return method == 3
+    return method.upper() == "HEAD"
+
+
+# In case there is a Session object made before importing requests_raw
+def __request(self, method, url, *args, **kwargs):
+    if not getattr(self, "_raw_adapter_mounted", False):
+        self.mount("http://", RawAdapter())
+        self.mount("https://", RawAdapter())
+        setattr(self, "_raw_adapter_mounted", True)
+    return _request(self, method, url, *args, **kwargs)
+
 
 def monkey_patch_all():
     setattr(requests, "raw", raw)
     setattr(requests.api, "raw", raw)
-    setattr(requests, "Session", Session)
-    setattr(requests.sessions, "Session", Session)
+    setattr(requests.sessions.Session, "raw", Session.raw)
+    setattr(requests.sessions.Session, "request", __request)
+    setattr(requests.packages.urllib3.response, "is_response_to_head", __is_response_to_head)
+
+
+# Monkey Patch Automatically
+monkey_patch_all()
--- requests_raw/api.py
+++ requests_raw/api.py
@@ -2,6 +2,11 @@ from .sessions import Session
 from .__version__ import __title__
 
 
-def raw(url, data, **kwargs):
+def request(method, url, **kwargs):
     with Session() as session:
-        return session.request(__title__, url, data=data, **kwargs)
+        return session.request(method=method, url=url, **kwargs)
+
+
+def raw(url, data, **kwargs):
+    kwargs.setdefault('allow_redirects', True)
+    return request(__title__, url, data=data, **kwargs)
