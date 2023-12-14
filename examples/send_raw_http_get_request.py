710d747e8bbc6cd1460f58b4078cb568a1e1ec9e
Tomer Zait <realgam3@gmail.com>
2023-12-15 00:26:18 +0200

    working state

--- examples/send_raw_http_get_request.py
+++ examples/send_raw_http_get_request.py
@@ -2,6 +2,6 @@
 
 import requests_raw
 
-res = requests_raw.raw(url='http://httpbin.org:80/', data=open("get_request.txt", "rb").read())
+res = requests_raw.raw(url='https://httpbin.org/', data=b"GET /get HTTP/1.1\r\nHost: httpbin.org\r\n\r\n")
 res_json = res.json()
 print(res_json)
--- requests_raw/connection.py
+++ requests_raw/connection.py
@@ -1,71 +1,56 @@
+import typing
+from http import client
 from .__version__ import __title__
-from requests.utils import urlparse
-from urllib3.connection import HTTPConnection, HTTPSConnection
-
-try:
-    from http import client
-except ImportError:
-    import httplib as client
+from urllib.parse import urlparse
+from urllib3.connection import HTTPConnection, HTTPSConnection, _TYPE_BODY
 
 
 class RawHTTPConnection(HTTPConnection):
     def __init__(self, *args, **kwargs):
-        super(RawHTTPConnection, self).__init__(*args, **kwargs)
-        self.__url = None
+        super().__init__(*args, **kwargs)
         self.__method = None
 
-    def putrequest(self, method, url, *args, **kwargs):
+    def request(
+            self,
+            method: str,
+            url: str,
+            body: _TYPE_BODY | None = None,
+            headers: typing.Mapping[str, str] | None = None,
+            *,
+            chunked: bool = False,
+            preload_content: bool = True,
+            decode_content: bool = True,
+            enforce_content_length: bool = True,
+    ) -> None:
         self.__method = method.lower()
+        if self.__method == __title__:
+            # HTTP Proxy
+            _url = urlparse(url)
+            if _url.scheme and _url.netloc:
+                _body = body.split(b"/", 1)
+                if _body[0].endswith(b" "):
+                    _body.insert(1, "{url.scheme}://{url.netloc}/".format(url=_url).encode())
+                else:
+                    _body.insert(1, b"/")
+                body = b"".join(_body)
+
+        return super().request(
+            method, url, body, headers,
+            chunked=chunked, preload_content=preload_content,
+            decode_content=decode_content, enforce_content_length=enforce_content_length
+        )
+
+    def putrequest(
+            self,
+            method: str,
+            url: str,
+            skip_host: bool = False,
+            skip_accept_encoding: bool = False,
+    ) -> None:
         if self.__method != __title__:
-            return super(RawHTTPConnection, self).putrequest(
-                method, url, *args, **kwargs
-            )
-
-        self.__url = urlparse(url)
-        if self._HTTPConnection__response and self._HTTPConnection__response.isclosed():
-            self._HTTPConnection__response = None
-
-        if self._HTTPConnection__state == client._CS_IDLE:
-            self._HTTPConnection__state = client._CS_REQ_STARTED
-        else:
-            raise client.CannotSendRequest(self._HTTPConnection__state)
-
-    def putheader(self, header, *values):
-        if self.__method != __title__:
-            return super(RawHTTPConnection, self).putheader(header, *values)
-
-        if self._HTTPConnection__state != client._CS_REQ_STARTED:
-            raise client.CannotSendHeader()
-
-    def endheaders(self, message_body=None, **kwargs):
-        if self.__method != __title__:
-            return super(RawHTTPConnection, self).endheaders(message_body=message_body, **kwargs)
-
-        buffer = message_body
-        # HTTP Proxy
-        if self.__url.scheme and self.__url.netloc:
-            _buffer = buffer.split(b"/", 1)
-            if _buffer[0].endswith(b" "):
-                _buffer.insert(1, "{url.scheme}://{url.netloc}/".format(url=self.__url).encode())
-            else:
-                _buffer.insert(1, b"/")
-            buffer = b"".join(_buffer)
-        if self._HTTPConnection__state == client._CS_REQ_STARTED:
-            self._HTTPConnection__state = client._CS_REQ_SENT
-        else:
-            raise client.CannotSendHeader()
-        self.send(buffer)
-
-
-class RawHTTPSConnection(HTTPSConnection):
-    def __init__(self, *args, **kwargs):
-        super(RawHTTPSConnection, self).__init__(*args, **kwargs)
-
-    def putrequest(self, method, url, *args, **kwargs):
-        self.__method = method.lower()
-        if self.__method != __title__:
-            return super(RawHTTPSConnection, self).putrequest(
-                method, url, *args, **kwargs
+            return super().putrequest(
+                method, url,
+                skip_host=skip_host, skip_accept_encoding=skip_accept_encoding
             )
 
         if self._HTTPConnection__response and self._HTTPConnection__response.isclosed():
@@ -76,19 +61,22 @@ class RawHTTPSConnection(HTTPSConnection):
         else:
             raise client.CannotSendRequest(self._HTTPConnection__state)
 
-    def putheader(self, header, *values):
+    def putheader(self, header: str, *values: str) -> None:
         if self.__method != __title__:
-            return super(RawHTTPSConnection, self).putheader(header, *values)
+            return super().putheader(header, *values)
 
         if self._HTTPConnection__state != client._CS_REQ_STARTED:
             raise client.CannotSendHeader()
 
     def endheaders(self, message_body=None, **kwargs):
         if self.__method != __title__:
-            return super(RawHTTPSConnection, self).endheaders(message_body=message_body, **kwargs)
+            return super().endheaders(message_body=message_body, **kwargs)
 
         if self._HTTPConnection__state == client._CS_REQ_STARTED:
             self._HTTPConnection__state = client._CS_REQ_SENT
         else:
             raise client.CannotSendHeader()
-        self.send(message_body)
+
+
+class RawHTTPSConnection(RawHTTPConnection, HTTPSConnection):
+    pass
